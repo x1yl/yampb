ab5949c657cbf0ce2f347744d7d0d97817399d88
x1yl <106912268+x1yl@users.noreply.github.com>
2022-11-13 03:42:15 +0000

    yes

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #place token between ''
 #DO NOT UNDER ANY CIRCUMSTANCE SHARE YOUR TOKEN
-token = 'MTA0MTA2MDAwODQ5NTkzOTc2Ng.G11egV.5rmRCQr2TJKDeQ17CW6bkzgiDX8CPCY3nGS8yg'
+token = 'MTA0MTA2MDAwODQ5NTkzOTc2Ng.GgI9ln.I8xZbRwDzRi1dVwKHOF1Bxkf-e8dkW79bvoyNw'
 #change prefix through discord (!changeprefix or /changeprefix)
 prefix = '!'
