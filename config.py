ec5c0d4e06807c31b9e65e75f0cd2e0af684400a
x1yl <106912268+x1yl@users.noreply.github.com>
2022-11-12 22:03:42 -0500

    removed token

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #place token between ''
 #DO NOT UNDER ANY CIRCUMSTANCE SHARE YOUR TOKEN
-token = 'MTA0MTA2MDAwODQ5NTkzOTc2Ng.Ga-i6P.nvxTUwKmOrj3stmIf2Z1dw2u5fEE0WKwWSFx64'
+token = ''
 #change prefix through discord (!changeprefix or /changeprefix)
 prefix = '!'
