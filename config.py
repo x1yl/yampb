92c2779227d746e61558a1f861310afe71faed4c
x1yl <106912268+x1yl@users.noreply.github.com>
2022-11-16 02:07:18 +0000

    testings

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #place token between ''
 #DO NOT UNDER ANY CIRCUMSTANCE SHARE YOUR TOKEN
-token = 'MTA0MTA2MDAwODQ5NTkzOTc2Ng.GwUUU9.vm3UBr_aO1AhpkqZuINJlyvqyeJ5CP2_SUFSnY'
+token = ''
 #change prefix through discord (!changeprefix or /changeprefix)
 prefix = '!'
